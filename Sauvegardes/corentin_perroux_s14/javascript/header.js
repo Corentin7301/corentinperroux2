2055d4b8d1f4f970c425affb10c8ef08062bb8cc
corentinperroux <corentin7301@gmail.com>
2020-12-20 14:09:31 +0100

    delete Sauvegardes

--- Sauvegardes/corentin_perroux_s14/javascript/header.js
+++
@@ -1,4 +0,0 @@
-let image_header = document.querySelector('.img-head')
-
-
-image_header.style.display = 'block';
--- Sauvegardes/corentin_perroux_s16/javascript/header.js
+++
@@ -1,5 +0,0 @@
-let image_header = document.querySelector('.img-head img')
-
-
-image_header.style.display = 'block';
-

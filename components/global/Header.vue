21a0a4d009105a99dfe2fa45c29de4c8c535a05a
corentinperroux <corentin7301@gmail.com>
2020-12-23 15:47:10 +0100

    top

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -12,7 +12,7 @@
             <button id="column2" type="button"><img src="../images/icons/columns2-solid_blanc2.png"
                     alt="columns"></button>
         </div> -->
-    <a href="#top"><svg class="w-6 h-6 top" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+    <a href="#top" :class="displayShow"><svg class="w-6 h-6 top" fill="none" stroke="currentColor" viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11l7-7 7 7M5 19l7-7 7 7">
         </path>
@@ -22,11 +22,35 @@
 
 <script>
   export default {
+    props: ['display'],
     data() {
       return {
+        scrollY: 0,
+        // will be hide after 350 px scroll down
+        hideFrom: 350
+      }
+    },
+    methods: {
+      setScrollY() {
+        this.scrollY = window.scrollY;
+      }
+    },
+    computed: {
+      // fixed || none
+      displayShow() {
+          if (this.scrollY < this.hideFrom) {
+            return "none"
+          } else {
+            return "fixed"
+          }
 
       }
     },
+    mounted() {
+      window.addEventListener('scroll', (event) => {
+        this.setScrollY()
+      })
+    }
   }
 
 </script>
@@ -60,6 +84,19 @@
       font-size: 11px;
       font-weight: 400;
       letter-spacing: 0.45em;
+    }
+
+    .fixed {
+      position: fixed;
+      width: 25px;
+      height: 25px;
+      bottom: 50px;
+      right: 30px;
+      z-index: 99;
+    }
+
+    .none {
+      display: none;
     }
   }
 

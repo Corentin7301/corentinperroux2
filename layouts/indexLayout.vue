daf79293dcecf54875bc0bf6e84e514f0c714f27
corentinperroux <corentin7301@gmail.com>
2021-01-10 15:16:51 +0100

    img head mobile

--- layouts/indexLayout.vue
+++ layouts/indexLayout.vue
@@ -2,8 +2,8 @@
   <div>
     <div class="img_head">
 
-    <cld-image public-id="corentinperroux.fr/corentinperroux_sighg8.jpg" width="100%" loading="lazy" fetchFormat="auto" quality="auto"
-        alt="Corentin-PERROUX" id="img_head" />
+      <cld-image public-id="corentinperroux.fr/corentinperroux_sighg8.jpg" width="100%" loading="lazy"
+        fetchFormat="auto" quality="auto" alt="Corentin-PERROUX" id="img_head" />
     </div>
     <Header />
     <Nuxt />
@@ -25,16 +25,23 @@
 
 
 <style lang="scss" scoped>
-
-.img_head {
-    height: 615px;
+  .img_head {
+    margin-bottom: 20px;
     overflow: hidden;
     display: flex;
     justify-content: center;
     align-items: center;
+
     #img_head {
       width: 100%;
     }
-}
+  }
+
+  @media screen and (min-width: 1216px) {
+    .img_head {
+      height: 615px;
+      margin: 0;
+    }
+  }
 
 </style>

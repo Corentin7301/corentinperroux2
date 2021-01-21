883610187a23cd4c1cbc0e2b64c7e940809b28ae
corentinperroux <corentin7301@gmail.com>
2021-01-21 11:10:27 +0100

    copyright

--- components/global/Copyright.vue
+++ components/global/Copyright.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <p>© Copyright <a href="https://portfolio.corentinperroux.fr" target="_blank">Corentin PERROUX </a>2020</p>
+  <p>© Copyright <a href="https://portfolio.corentinperroux.fr" target="_blank">Corentin PERROUX </a>2020 || Photos non libres de droit</p>
 </div>
 </template>
 

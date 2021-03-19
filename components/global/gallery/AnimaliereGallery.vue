e8672debf80649311d058e84088bf744dfcbc9a1
corentinperroux <corentin7301@gmail.com>
2021-03-19 10:38:17 +0100

    add photo in artistique

--- components/global/gallery/AnimaliereGallery.vue
+++ components/global/gallery/AnimaliereGallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="gallery">
-    <div v-for="photo in photos" :key="photo.id" class="content">
+    <div v-for="photo in photos" :key="photo.id" ref="" class="content">
       <a :href="'https://res.cloudinary.com/corentin7301/image/upload/f_auto,q_auto/v1/' + path + photo.url + jpg" data-lightbox="animaliere"><cld-image :public-id="path + photo.url + jpg" width="100%" loading="lazy" fetchFormat="auto" quality="auto"
         :alt="photo.description" class="photo" /></a>
     </div>
